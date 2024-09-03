d7a212d33cc774480a061afc51e152cc17e46e85
CINECUBE <64253420+CINECUBE@users.noreply.github.com>
2024-09-04 01:03:16 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.lycinema.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "AHQPpPdMSz3NVEGIf7LgzvQgOL34Lpq3KT4I0KvP8Js=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
